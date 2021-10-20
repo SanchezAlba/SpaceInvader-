ae08f32847f91802f4d7583e03fce9567495714e
SanchezAlba <albisanchez2003@gmail.com>
2021-10-20 17:43:01 +0100

    arreglitos
    cambia de pantalla

--- SpaceInvader unityhub/Assets/Scripts/GameController.cs
+++ SpaceInvader unityhub/Assets/Scripts/GameController.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameController : MonoBehaviour
 {
     public GameObject introScreen;
     public GameObject pantallaEspera;
+    public GameObject pantallaInicial;
+    public float time = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,21 +20,42 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKey) // al pulsar cualquier tecla cambie la pantalla
         {
-            Debug.Log("esto furula");
+            EnablePantallaInicial();
+            DisablePantallaEspera();
+        }
+
+        time = time + Time.deltaTime; // cuando acabe el texto se cambie la pantalla
+        if(time>=41.0f)
+        {
             EnablePanatallaEspera();
             DisableIntroScreen();
         }
+
+      
     }
 
     void EnablePanatallaEspera()
     {
         pantallaEspera.SetActive(true);
     }
+    void DisablePantallaEspera()
+    {
+        pantallaEspera.SetActive(false);
+    }
+
     void DisableIntroScreen()
     {
         introScreen.SetActive(false);
     }
 
+    void EnablePantallaInicial()
+    {
+        pantallaInicial.SetActive(true);
+    }
+    void DisablePantallaInicial()
+    {
+        pantallaInicial.SetActive(false);
+    }
 }
