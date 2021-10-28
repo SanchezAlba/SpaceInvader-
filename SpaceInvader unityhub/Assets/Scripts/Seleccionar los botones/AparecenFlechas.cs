03499ab24bab1a9c567d1cbcc515056ca5c736d7
SanchezAlba <90839003+SanchezAlba@users.noreply.github.com>
2021-10-28 14:25:27 +0200

    estrellas
    mmm

---
+++ SpaceInvader unityhub/Assets/Scripts/Seleccionar los botones/AparecenFlechas.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AparecenFlechas : MonoBehaviour
+{
+    public GameObject boton;
+    public GameObject botonUno;
+    public GameObject botonDos;
+    public GameObject jugar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivarBoton()
+    {
+        
+    }
+
+}
