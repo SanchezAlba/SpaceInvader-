2d6e256555aec36836c62432d2f94b95a4c82c49
SanchezAlba <albisanchez2003@gmail.com>
2021-11-11 17:36:46 +0000

    los efectos
    slider efectos

--- SpaceInvader unityhub/Assets/Scripts/Sliders/MusicSource.cs
+++ SpaceInvader unityhub/Assets/Scripts/Sliders/MusicSource.cs
@@ -5,47 +5,41 @@ using UnityEngine.UI;
 
 public class MusicSource : MonoBehaviour
 {
+    public static MusicSource instance;
+
     public Slider sliderMusic; //para tener acceso al valor
     public Slider sliderEffects;
+   
 
     public AudioSource musicSource;
-    public AudioSource effectsSource;
-    
+    public AudioSource effectsSource;  
 
-    public Image imagenMute;
+  
+
+    void Awake()
+    {
+        instance = this;
+        InitializeVolume();
+    }
 
     private void InitializeVolume()
     {
-        effectsSource.volume = PlayerPrefs.GetFloat("effectsVolumen", 1.0f);
-        musicSource.volume = PlayerPrefs.GetFloat("musicVolumen", 1.0f);
-
         sliderMusic.value = musicSource.volume;
         sliderEffects.value = effectsSource.volume;
+
+
     }
 
   public void SliderMusicModified()
     {
         musicSource.volume = sliderMusic.value;
-        PlayerPrefs.SetFloat("musicVolumen", musicSource.volume);
-        PlayerPrefs.Save();
+       
     }
 
     public void SliderEffectsModified()
     {
-        musicSource.volume = sliderEffects.value;
-        PlayerPrefs.SetFloat("effectsVolumen", effectsSource.volume);
-        PlayerPrefs.Save();
+        effectsSource.volume = sliderEffects.value;
+        
     }
-
-    /*public void RevisarSiEstoyMute()
-    {
-        if(sliderMusic == 0)
-        {
-            imagenMute.enabled = true;
-        }
-        else
-        {
-            imagenMuete.enable = false;
-        }
-    }*/
+   
 }
