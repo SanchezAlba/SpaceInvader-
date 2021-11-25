50fd670f340140cf928a870e2483040cfb3e7cb0
SanchezAlba <albisanchez2003@gmail.com>
2021-11-25 14:24:35 +0000

    idiomas 2
    cambiar con botones

---
+++ SpaceInvader unityhub/Assets/Scripts/Idiomas/ChangeLanguage.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class ChangeLanguage : MonoBehaviour
+{
+    public int index = 0;
+
+    private IEnumerator Start()
+    {
+        // espera hasta que el plugin está cargado
+        yield return LocalizationSettings.InitializationOperation;
+
+        // VAmos a buscar el índice idioma seleccionado por defecto
+        Locale search = LocalizationSettings.AvailableLocales.Locales[index];
+
+        while (search != LocalizationSettings.SelectedLocale && index < LocalizationSettings.AvailableLocales.Locales.Count);
+        {
+            index++;
+            search = LocalizationSettings.AvailableLocales.Locales[index];
+        }
+    }
+
+    public void NextLanguage()
+    {
+        index++;
+        if (index>2)
+        {
+            index = 0;
+        }
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+
+    public void PreviusLanguage()
+    {
+        index--;
+        if (index<0)
+        {
+            index = 2;
+        }
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+
+}
