60a4f176d042a42b610e17a2b221f73586204b84
SanchezAlba <albisanchez2003@gmail.com>
2022-02-23 20:21:47 +0100

    + script
    m

--- SpaceInvader unityhub/Assets/Scripts/Juego_Scene nueva/PlayerLoader.cs
+++ SpaceInvader unityhub/Assets/Scripts/Juego_Scene nueva/PlayerLoader.cs
@@ -11,6 +11,9 @@ public class PlayerLoader : MonoBehaviour
       GameObject nave = Instantiate(GameDataPersistent.instance.selectedSpaceship.prefab); //ponerlo public y meter al prefab
                                                                                            // nave.transform.localPosition()  //para decir en que posicio lo queremos
 
+        nave.transform.position = new Vector3(-39,30, 22 );
+        nave.transform.localScale *= 10;
+
        //nave.transform.parent = transform; // esto es por si lo queremos meter dentro de un  empty. Pues para que aparezca ahí
     }
 }
--- SpaceInvader unityhub/Assets/Scripts/Naves/MovimientoNaves.cs
+++ SpaceInvader unityhub/Assets/Scripts/Naves/MovimientoNaves.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class MovimientoNaves : MonoBehaviour
 {
+    public float speed1 = 3f;
 
 
     void Start()
     {
-        
+        Application.targetFrameRate = 30;
     }
 
     
     void Update()
     {
-        
+        Vector3 myMove = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            myMove += Vector3.right * speed1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myMove += Vector3.left * speed1;
+        }
     }
 }
