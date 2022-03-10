cd1ff0b35e44dbbc55a7bd663480e4eab213d9fa
SanchezAlba <albisanchez2003@gmail.com>
2022-03-10 20:13:45 +0100

    q
    s

---
+++ SpaceInvader unityhub/Assets/Scripts/Escudos/ScudosController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScudosController : MonoBehaviour
+{
+    /*public int collisionCount;
+    private int duracion;
+
+    public GameObject escudo1;
+    public GameObject escudo2;
+    public GameObject escudo3;
+    public GameObject escudo4;
+    public GameObject escudo5;
+
+    void Start()
+    {
+        duracion = GameDataPersistent.instance.selectedSpaceship.shield;
+        collisionCount = duracion;
+    }
+
+    private void Update()
+    {
+        Debug.Log(collisionCount);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        collisionCount--;
+
+        if (collisionCount == 5)
+        {
+            escudo5.SetActive(true);
+            escudo3.SetActive(false);
+            escudo2.SetActive(false);
+            escudo1.SetActive(false);
+            escudo4.SetActive(false);
+        }
+
+        if (collisionCount == 4)
+        {
+            escudo5.SetActive(false);
+            escudo4.SetActive(true);
+            escudo3.SetActive(false);
+            escudo2.SetActive(false);
+            escudo1.SetActive(false);
+            
+        }
+
+        if (collisionCount == 3)
+        {
+            escudo5.SetActive(false);
+            escudo4.SetActive(false);
+            escudo3.SetActive(true);
+            escudo2.SetActive(false);
+            escudo1.SetActive(false);
+        }
+
+        if (collisionCount == 2)
+        {
+            escudo5.SetActive(false);
+            escudo4.SetActive(false);
+            escudo3.SetActive(false);
+            escudo2.SetActive(true);
+            escudo1.SetActive(false);
+        }
+
+        if (collisionCount == 1)
+        {
+            escudo5.SetActive(false);
+            escudo4.SetActive(false);
+            escudo3.SetActive(false);
+            escudo2.SetActive(false);
+            escudo1.SetActive(true);
+        }
+
+        if (collisionCount == 0)
+        {
+            //escudo5.SetActive(false);
+            //escudo4.SetActive(false);
+            //escudo3.SetActive(false);
+            //escudo2.SetActive(false);
+            escudo1.SetActive(false);
+        }
+
+       
+
+    }*/
+
+}
